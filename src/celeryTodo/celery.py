520a93e8e9b2dea7dec8a2322f866f97813630c7
eyakub <eyakubsorkar@gmail.com>
2019-09-24 15:41:00 +0600

    pushing celery config

---
+++ src/celeryTodo/celery.py
@@ -0,0 +1,22 @@
+from __future__ import absolute_import
+import os
+from celery import Celery
+
+
+# set the default Django settings modules for the 'celery proejct'
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'celeryProject.settings')
+
+app = Celery('celeryProject')
+
+# using a string here means the worker doesn't have to serialize
+# the configuration object to child processes.
+# - namespace = "CELERY" means all celery related configuration key
+# should have a 'CELERY' prefix
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+# load task modules from all registered Django app configs
+app.autodiscover_tasks()
+
+@app.task(bind=True)
+def debug_task(self):
+    print('Request: {0!r}'.format(self.request))
