3a34032560b9d24dda975dfcfabd69923a001451
eyakub <eyakubsorkar@gmail.com>
2019-09-26 11:50:29 +0600

    solved left issues about celery

--- src/celeryTodo/tasks.py
+++ src/celeryTodo/tasks.py
@@ -2,6 +2,9 @@ from __future__ import absolute_import, unicode_literals
 from celery import shared_task
 from time import sleep
 from django.core.mail import send_mail
+from django.utils.crypto import get_random_string
+import string
+from django.contrib.auth.models import User
 
 
 @shared_task
@@ -46,3 +49,13 @@ def send_email_task():
         ['doxeb@247web.net']
     )
     return None
+
+
+@shared_task
+def create_random_user_accounts(total):
+    for i in range(total):
+        username = 'user_{}'.format(get_random_string(10, string.ascii_letters))
+        email = '{}@example.com'.format(username)
+        password = get_random_string(50)
+        User.objects.create_user(username=username, email=email, password=password)
+    return '{} random users created with success!'.format(total)
--- src/celeryTodo/urls.py
+++ src/celeryTodo/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
+from django.conf.urls import url
 from . import views
 
 
 urlpatterns = [
-    path('', views.index, name='index')
+    #path('', views.index, name='index'),
+    url(r'^$', views.UsersListView.as_view(), name='users_list'),
+    url(r'^generate/$', views.GenerateRandomUserView.as_view(), name='generate'),
+
 ]
